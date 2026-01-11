6865237d9db2f8653fb8cca576075753c5a2f77a
unikone402 <139197439+unikone402@users.noreply.github.com>
2026-01-11 21:49:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BankingApp from './MockBank'
-import './index.css' // Optional: if you have global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
